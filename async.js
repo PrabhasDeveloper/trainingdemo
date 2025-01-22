1b5281698afd8cd6938c95377176e60e928679d4
PrabhasDeveloper <158133465+PrabhasDeveloper@users.noreply.github.com>
2025-01-22 12:59:58 +0530

    Add files via upload

--- async.js
+++ async.js
@@ -1,21 +1,49 @@
-// console.log(10);
-// console.log(20);
-// // setTimeout(()=>{
-// //    console.lOg(30); 
-// // },5000)
+// // console.log(10);
+// // console.log(20);
+// // // setTimeout(()=>{
+// // //    console.lOg(30); 
+// // // },5000)
 
-// setInterval(()=>{
-//    // document.writeln("MRU");
-//    console.lOg(30);
-// },1500)
-// console.log(40);
-// console.log(50);
+// // setInterval(()=>{
+// //    // document.writeln("MRU");
+// //    console.lOg(30);
+// // },1500)
+// // console.log(40);
+// // console.log(50);
 
-// let p1 = new Promise((resolve , reject )=>{});
-// console.log(p1);
+// // let p1 = new Promise((resolve , reject )=>{});
+// // console.log(p1);
 
-let p2 = new Promise((resolve,reject)=>{
-    resolve("sucess");
-})
-p2.then((response)=>console.log(response))
-// console.log(p2);
+// let p2 = new Promise((resolve,reject)=>{
+//     resolve("sucess");
+// })
+// p2.then((response)=>console.log(response))
+// // console.log(p2);
+
+// ! Api fetching
+
+
+function  fecthusers (){
+
+    let response = fetch("https://jsonplaceholder.typicode.com/users");
+
+    response.then(result => {
+        return result.json().then(data => {
+            console.log(data);
+            let store = document.getElementById("store");
+            console.log(store);
+            data.map(user=>{
+                store.innerHTML += `
+                <tr>
+                <td>${user.id}</td>
+                 <td>${user.name}</td>
+                  <td>${user.email}</td>
+                   <td>${user.company.name}</td>
+                </tr>
+                `
+            })
+        })
+    })
+    .catch(err=> console.log(err))
+}
+fecthusers();
