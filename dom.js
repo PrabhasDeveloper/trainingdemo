83ee9cee2fbf1adf98d03cc8cf4caffe48b9d556
PrabhasDeveloper <158133465+PrabhasDeveloper@users.noreply.github.com>
2025-01-24 12:57:24 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,2 +1,23 @@
-let element = document.getElementById("demo");
-console.log(element);
+// let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="hello"
+
+
+
+// let x = [...ele];
+// x.map(element => {
+//     element.style.backgroundColor="Orange"
+// })
+
+
+
+// let ele =  document.querySelector(" #mru");
+// console.log(ele);
+
+// let ele =  document.querySelectorAll(".test");
+// console.log(ele);
+
+let  btn  = doucument.querySelector("#btn");
+btn.addEventListener("click",())
+
